6d805b7a77d798fa03fbc55eb98644dc5b3f116e
Bort Edwards <42812596+BortEdwards@users.noreply.github.com>
2021-01-27 16:04:25 -0500

    Update Cleaning database downloads.R

--- Cleaning database downloads.R
+++ Cleaning database downloads.R
@@ -1,31 +1,28 @@
-# tools already exist for cleaning data and text, eg: https://www.rdocumentation.org/packages/textclean/versions/0.9.3
-# however given the particular issues encountered addressing plant collection data bespoke code was can be required.
-# This code addresses some very specific problems to data extracted from the Global Compositae Database (2018) however many of these tools can be easily adapted to other problematic datasets
-# NB encoding system can horribly confuse this process. In this case take care to check correct recognition of input, conversion to and subsequent use of, UTF8
-
-library(stringr)
-library(data.table)
-library(stringr)
-library(dplyr)
-library(data.table)
-library(utf8)
-
-setwd("/Users/vicki/Dropbox (Smithsonian)/Yale/Comps")
-df.corrupt <- fread("WORKING Global Comps Master SpreadsheetUTF8.txt", quote="")
 
 ### ------ Fix offset columns and flag missing values ------ ###
+# In this example the dataset (see bottom for dummy set) has 5 columns, the first two of which are fine but
+# Column 3: varying length string representing a species name and author(s) - sometimes missing some text that has been split into col(s) 4 (and sometimes 5) - it looks like left over commas at some point split the text but are no longer in the data.
+# Column 4: validity of name as a single word - should be either "Accepted", "Synonym", or "NULL" but in some cases is empty and in worst cases has text that should be in col 3.
+# Column 5: Accepted name - Should contain valid binomial and author (similar to column 3). The contents of column 5 should be: the same as column 3 if column 4 = "Accepted"; different if "Synonym"; and "NULL" if "Unknown") but sometimes has fragments of text from column 3, and is sometimes empty (and cannot be inferred from other columns here).
 
-# Set data.bad as data.table
+
+# To fix this need to identify cells in "Status" with a valid entry, and then add a blank cell to "Status" for rows without a valid value.
+# These cells were then populated with a "FLAG" flag for further scrutiny.
+
+library(data.table)
+
+#convert dataframe to data.table for easier manipulation
 setDT(df.corrupt)
-# Define valid values for column "Status"
+
+# Define existing VALID values for column "Status"
 valid_status <- c("Accepted", "Synonym", "Unknown")
 
 # Join invalid status to NameFull column
 df.corrupt[!Status %in% valid_status, NameFull := paste(NameFull, Status, sep = ", ")]
-# then flag invalid status
+# then add "flag" to cells with invalid status
 df.corrupt[!Status %in% valid_status, Status := "FLAG"]
 
-# Combine contents of two or more cells conditional upon third cell value. ie Join AcceptedName to NameFull if Status == FLAG (and Accepted Name is not NULL)
+# Combine the contents of two or more cells conditional upon third cell value. ie join AcceptedName to NameFull if Status == FLAG (and Accepted Name is not NULL)
 df.corrupt[Status == "FLAG" & AcceptedName != "NULL", NameFull := paste(NameFull, AcceptedName, sep = ", ")]
 
 # Populate column ("Status") contingent on the value of other cells:
@@ -36,88 +33,6 @@ df.corrupt[Status == "FLAG" & !AcceptedName %in% c("FLAG", "NULL"), AcceptedName
 
 # return data table to dataframe format
 setDF(df.corrupt) #return data table to dataframe format
-
-
-### ------ Identify and replace corrupt text ------ ### 
-
-# replace multiple spaces with single spaces
-df.corrupt[] <- lapply(df.corrupt, function(x) (gsub('  +', ' ', x)))
-
-# Identify potentially corrupt characters
-exotic.chr.check <- setDT(df.corrupt)[, .(NameFull=unlist(str_extract_all(NameFull, "\\w+([^ -~]|\\?)\\w+"))), by = NameFull] #pull out all words with non-ASCII characters (^ to ~) and question marks https://stackoverflow.com/questions/43708888/regex-matching-utf-8-pattern-in-r
-exotic.chr.check <- select(exotic.chr.check,-c(1)) # Remove unwanted first column
-exotic.chr.check <- as.data.frame(unique(exotic.chr.check$NameFull)) # Reduce to unique instances of each word with undesirable character(s)
-
-# Identify question marks in species binomials - NB this does not identify "?" in subspecies or names outside the binomial
-matches <- grepl("\\?", df.corrupt$NameFull) # creates a logical vector for each row: TRUE = match, FALSE = no match
-df.hits <- df.corrupt[matches,] # lists all cells with "?"
-
-# For plant names a leading "?" in a binomial typically indicates a missing/corrupt "×" designating hybrid status. "×" is a multiplication sign (U+2715) NOT a clear-text "x"
-# To substitute "these"?" for "×" in binomials:
-df.corrupt$NameFull <- str_replace_all(df.corrupt$NameFull, "^\\?|(?<=^(\\?)?\\b\\w{1,100}\\b\\s)\\?", "×") # replaces "?" in binomial (first two words) with "×" as hybrid designation
-df.corrupt$AcceptedName <- str_replace_all(df.corrupt$AcceptedName, "^\\?|(?<=^(\\?)?\\b\\w{1,100}\\b\\s)\\?", "×") # replaces "?" in binomial (first two words) with "×" as hybrid designation
-
-matches <- grepl("\\?", df.corrupt$AcceptedName) # creates a logical vector for each row: TRUE = match, FALSE = no match
-df.author.hits <- df.corrupt[matches,] # lists all remaining cells with "?". This may be hybrid designation outside the binomial or other corruption in the authority names
-df.author.hits
-write.csv(df.author.hits,'corrupt.authority.entries.txt', fileEncoding = "UTF-16") # output corrupt records
-
-
-# To fix corrupted authority names you can either provide a list of each individual corrupted word/name and it's corrected version, each in quotes separated by a comma. NB special characters (eg "?" need to be escaped: "\?")
-# An alternative if you are confident each corrupt character corresponds to a single "correct" character is to parse a list like above but just for each character.
-list_gsub <- read.csv("conversion.listUTF16.txt", fileEncoding = "UTF-16",sep=",", header = TRUE) #this is a *UTF16 ENCODED* comma delimited look-up table - first column "corrupted" is the corrupt name (in quotes) and a corresponding entry in second column "clean" is the clean version. 
-list_gsub$clean <- as_utf8(list_gsub$clean) # convert utf16-8 using utf8 package - this removes external code conversion issues
-list_gsub$corrupted <- as_utf8(list_gsub$corrupted)
-
-# For unknown reasons my corrupt dataset has replaced "á" with non-breaking spaces. Replace using  unicode designation for non-breakingspace.                                                      
-df.clean <- df.corrupt
-df.clean$NameFull <- (gsub("\u00A0", "á", df.clean$NameFull, fixed = TRUE))
-df.clean$AcceptedName <- (gsub("\u00A0", "á", df.clean$AcceptedName, fixed = TRUE))
-
-# loop through each lookup table entry and fix for column "NameFull https://stackoverflow.com/questions/24058700/find-and-replace-words
-for(x in 1:nrow(list_gsub))
-  df.clean$NameFull <- gsub(list_gsub[x,"corrupted"],list_gsub[x,"clean"], df.clean$NameFull)
-
-# repeat for column "AcceptedName"
-for(x in 1:nrow(list_gsub))
-  df.clean$AcceptedName <- gsub(list_gsub[x,"corrupted"],list_gsub[x,"clean"], df.clean$AcceptedName)
-
-#It is worth running the "clean" dataframe back through the above "df.corrupt <- df.clean" to check errors or overlooked characters.
-
-write.table(df.clean,'WORKING Global Comps Master Spreadsheet CLEAN.txt', sep="\t", quote=FALSE, row.names = FALSE) # output corrupt records
-
-
-### ------ Options ------ ### 
-
-
-## To create lookup table in-script rather than importing a list:
-list_gsub2 <- read.csv(text="
-corrupted,clean
-,ü
-¢,ó
-‰,ë
-¤,ñ
-‡,ç
-†,å
-•,ò
-”,ö
-„,ä
-Œ,î
-ƒ,â
-ˆ,ê")
-
-##This is an example of corrupt characters from a dump of the Global Compositae Database:
-
-
-##BEWARE! Not all characters are a one-to-one substitution. In the example of the GCD "?" substitutes for a host of special characters:
-#? = ×,ı,Á,ă,ą,ã,ě,ę,ł,ý,ř,š,ş,Š,Ś,ø,Ó,Ø,ń,ğ,Č,ć,č,ž,'U
-
-## Also, characters that may be valid in one instance can represent a corrupt character elsewhere, eg:
-#š = Ü
-#‚ = é
-#Š = è
-
-##In these cases I see no good solution other than writing a case-by-case list to parse through the script. The first steps of this script can be used to identify these cases.
 
 
 ### ------ Toy corrupt dataset ------ ###
